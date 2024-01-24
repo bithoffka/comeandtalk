2aee8e76df2d77f87a160632b390f85ccdacf924
bithoffka <103552516+bithoffka@users.noreply.github.com>
2024-01-24 21:19:37 +0300

    Add files via upload

---
+++ catbot.py
@@ -0,0 +1,199 @@
+import logging
+from aiogram import types, Bot, Dispatcher, executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+from markups import *
+
+token = "6100825136:AAHwtNxu-kaHE2K2aGuslJEclVSZPtyRtm8"
+#Test token - 6100825136:AAHwtNxu-kaHE2K2aGuslJEclVSZPtyRtm8
+
+bot = Bot(token)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+logging.basicConfig(level=logging.INFO)
+
+manager_id = "1440788864"
+
+class ApplicationStatesGroup(StatesGroup):
+	age_group = State()
+	direction = State()
+	class_type = State()
+	phone_number = State()
+
+def LogMessage(message):
+	logging.info(f"Message from {message.from_user.first_name}, user ID: {message.from_user.id}: {message.text}")
+
+def ChatTypePrivate(message): return message.chat.type == "private"
+
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+	if ChatTypePrivate(message) == True:
+		LogMessage(message)
+
+		markup_privacy = InlineKeyboardMarkup()
+		markup_privacy.add(InlineKeyboardButton(text="Политика Конфиденциальности", url="https://jazzy-dasik-1615a8.netlify.app/"))
+
+		await message.reply(f"Привет, {message.from_user.first_name}!\n🇬🇧Это бот Come & Talk🇪🇸\n\nЭтот бот поможет вам:\n🔰 Назначить урок\n🔰 Связаться с менеджером\n🔰 Узнать график работы\n🔰 Быть в курсе последних новостей\n🔰 Подписаться на наши социальные сети\n\nИ многое другое✅️\n\nℹ️Для списка команд выберите \"Помощь\" в меню\n\nℹ️Пользуясь ботом, вы соглашаетесь с Политикой Конфиденциальности, прочитать которую можно по кнопке ниже.", reply_markup=MainMarkup())
+		await message.answer("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓", reply_markup=markup_privacy)
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+@dp.message_handler(commands="sched")
+async def sched(message: types.Message):
+	if ChatTypePrivate(message) == True:
+		LogMessage(message)
+
+		await message.reply("⏰Часы работы всех наших центров развития⏰:\n\n📍 г.Михайловск, ул.Ленина, 111\n\n⏱ пн - 09:00–19:00\n⏱ вт - 09:00–19:00\n⏱ ср - 09:00–19:00\n⏱ чт - 09:00–19:00\n⏱ пт - 09:00–19:00\n⏱ сб - 09:00–19:00\n⏱ вс - 09:00–15:00\n\n📍 г.Михайловск, ул.Георгиевская,107\n\n⏱ пн - 09:00–19:00\n⏱ вт - 09:00–19:00\n⏱ ср - 09:00–19:00\n⏱ чт - 09:00–19:00\n⏱ пт - 09:00–19:00\n⏱ сб - 09:00–19:00\n⏱ вс - 09:00–15:00\n\n☎️ Номер телефона: 61-09-90, +7-(962)-451-09-90")
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+@dp.message_handler(commands="info")
+async def info(message: types.Message):
+	if ChatTypePrivate(message) == True:	
+		LogMessage(message)
+
+		await message.reply("ℹ️Информация\n\n📱Наш ВК: https://vk.com/comeandtalkk\n\n☎️ Номер телефона: 61-09-90, +7-(962)-451-09-90\n✉️ Почта: comeandtalk@yandex.ru\n🕸 Наш сайт: comeandtalk.ru")
+		await message.answer("ИП: Асирян Сатинэ Сергеевна\nИНН: 262303681451\nОГРН: 311265124400025\nВиды деятельности: 85.11, 85.12, 85.13, 85.14, 85.21, 85.22, 85.41, 85.42")
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+@dp.message_handler(commands="help")
+async def help(message: types.Message):
+	if ChatTypePrivate(message) == True:	
+		LogMessage(message)
+
+		await message.reply("🛠Помощь🛠\n\n/start - Перезапустить бота\n/signup - Записаться на занятие в нашем центре\n/sched - Раписание работы наших центров\n/info - Информация о нашей организации\n/help - Помощь")
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+@dp.message_handler(commands="signup")
+async def signup(message: types.Message):
+	if ChatTypePrivate(message) == True:	
+		LogMessage(message)
+
+		markup = InlineKeyboardMarkup()
+		markup.add(InlineKeyboardButton("Наш веб-сайт", url="https://comeandtalk.ru"))
+		await message.reply("📑Перед тем, как записаться, ознакомьтесь с программами обучения по кнопке снизу", reply_markup=markup)
+
+		await message.answer("🔰Выберите возраст🔰", reply_markup=AgeGroupMarkup())
+		await ApplicationStatesGroup.age_group.set()
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+#FSM HANDLERS START
+
+@dp.message_handler(state=ApplicationStatesGroup.age_group)
+async def fsm_age_group_handler(message: types.Message, state: FSMContext):
+	LogMessage(message)
+
+	async with state.proxy() as data:
+		if message.text == "👶Дети (4-7 лет)" or message.text == "👱Подростки (7-18 лет)" or message.text == "🧔‍♂️Взрослые (18-81 лет)":
+			data["age_group"] = message.text
+
+			await message.reply("🔰Выберите программу🔰", reply_markup=DirectionMarkup())
+			await ApplicationStatesGroup.next()
+		elif message.text == "🚫Отмена заявки🚫":
+			await message.reply("❌Заявка отменена успешно!", reply_markup=MainMarkup())
+			await state.finish()
+		else:
+			await message.reply("Воспользуйтесь кнопками ниже, чтобы установить возрастную категорию!")
+
+@dp.message_handler(state=ApplicationStatesGroup.direction)
+async def fsm_direction_handler(message: types.Message, state: FSMContext):
+	LogMessage(message)
+
+	async with state.proxy() as data:
+		if message.text == "🎓Интелектуальная" or message.text == "🥋Спортивная" or message.text == "🎨Творческая":
+			data["direction"] = message.text
+
+			age_group = data["age_group"]
+			direction = data["direction"]
+
+			next_markup = None
+
+			if age_group == "👶Дети (4-7 лет)":
+				if direction == "🎓Интелектуальная":
+					next_markup = KidsIntel()
+				elif direction == "🥋Спортивная":
+					next_markup = KidsSport()
+				elif direction == "🎨Творческая":
+					next_markup = KidsArt()
+			elif age_group == "👱Подростки (7-18 лет)":
+				if direction == "🎓Интелектуальная":
+					next_markup = TeensIntel()
+				elif direction == "🥋Спортивная":
+					next_markup = TeensSport()
+				elif direction == "🎨Творческая":
+					next_markup = TeensArt()
+			elif age_group == "🧔‍♂️Взрослые (18-81 лет)":
+				if direction == "🎓Интелектуальная":
+					next_markup = AdultsIntel()
+				elif direction == "🥋Спортивная":
+					next_markup = AdultsSport()
+				elif direction == "🎨Творческая":
+					next_markup = AdultsArt()
+
+			await message.reply("🔰Выберите тип занятия🔰", reply_markup=next_markup)
+			await ApplicationStatesGroup.next()
+		elif message.text == "🚫Отмена заявки🚫":
+			await message.reply("❌Заявка отменена успешно!", reply_markup=MainMarkup())
+			await state.finish()
+		else:
+			await message.reply("Воспользуйтесь кнопками ниже, чтобы выбрать программу!")
+
+@dp.message_handler(state=ApplicationStatesGroup.class_type)
+async def fsm_class_type_handler(message: types.Message, state: FSMContext):
+	LogMessage(message)
+
+	async with state.proxy() as data:
+		if message.text == "🇬🇧Английский язык" or message.text == "🔠Логопед" or message.text == "📚Подготовка к школе" or message.text == "💃Хореография" or message.text == "🧐Развитие" or message.text == "🔢Ментальная алгебра" or message.text == "📖Подготовка к ОГЭ/ЕГЭ/ВПР" or message.text == "🇦🇲Армянский язык" or message.text == "🇪🇸Испанский язык" or message.text == "🔊Хип-Хоп" or message.text == "🥋Карате" or message.text == "🎨Рисование" or message.text == "🎭Актерское мастерство" or message.text == "🖼Исскуствоведение" or message.text == "🇫🇷Французский язык" or message.text == "🎤Ораторское искуство" or message.text == "🖼Рисование" or message.text == "🧘Йога" or message.text == "💪Фитнес":
+			data["class_type"] = message.text
+
+			await message.reply("🔰Поделитесь контактом🔰", reply_markup=RequestContact())
+			await ApplicationStatesGroup.next()
+		elif message.text == "🚫Отмена заявки🚫":
+			await message.reply("❌Заявка отменена успешно!", reply_markup=MainMarkup())
+			await state.finish()
+		else:
+			await message.reply("Воспользуйтесь кнопками ниже, чтобы выбрать тип занятия!")
+
+@dp.message_handler(content_types=types.ContentType.CONTACT, state=ApplicationStatesGroup.phone_number)
+async def fsm_phone_number_handler(message: types.Message, state: FSMContext):
+	LogMessage(message)
+	
+	async with state.proxy() as data:
+		data["phone_number"] = message.contact.phone_number
+
+		await bot.send_message(manager_id, f'Заявка на регистрацию!\nID: {message.chat.id}\nUsername: {message.from_user.first_name}\nВозрастная категория: {data["age_group"]}\nПрограмма: {data["direction"]}\nТип занятий: {data["class_type"]}\nКонтакт: {data["phone_number"]}')
+
+	await message.reply("✅Готово, ваша заявка отправлена менеджеру, мы свяжемся с вами в ближайшее время!", reply_markup=MainMarkup())
+	await state.finish()
+
+#FSM HANDLERS END
+
+@dp.message_handler()
+async def handler(message: types.Message):
+	if ChatTypePrivate(message) == True:
+		text = message.text.strip()
+
+		if text == "🕰️График работы🕰️":
+			await sched(message)
+		elif text == "🔄Перезапустить бота🔄":
+			await start(message)
+		elif text == "📑Записаться на занятие📑":
+			await signup(message)
+		elif text == "ℹ️Информацияℹ️":
+			await info(message)
+		elif text == "🛠Помощь🛠":
+			await help(message)
+		else:
+			LogMessage(message)
+			await message.answer("Ваша команда не распознана, напишите /help для помощи, или нажмите кнопку \"Помощь\" внизу!")
+	else:
+		await message.reply("Бот не работает в группах, перейдите в приватный чат")
+
+if __name__ == "__main__":
+	executor.start_polling(dp, skip_updates=True)
---
+++ markups.py
@@ -0,0 +1,183 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+def MainMarkup():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+    start_button = KeyboardButton("🔄Перезапустить бота🔄")
+    signup_button = KeyboardButton("📑Записаться на занятие📑")
+    sched_button = KeyboardButton("🕰️График работы🕰️")
+    info_button = KeyboardButton("ℹ️Информацияℹ️")
+    help_button = KeyboardButton("🛠Помощь🛠")
+
+    markup.add(start_button)
+    markup.add(signup_button)
+    markup.add(sched_button)
+    markup.add(info_button, help_button)
+
+    return markup
+
+# AGE GROUP START
+
+def AgeGroupMarkup():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    kids_button = KeyboardButton("👶Дети (4-7 лет)")
+    teens_button = KeyboardButton("👱Подростки (7-18 лет)")
+    adults_button = KeyboardButton("🧔‍♂️Взрослые (18-81 лет)")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(kids_button, teens_button, adults_button, cancel_button)
+
+    return markup
+
+# AGE GROUP END
+
+# DIRECTION START
+
+def DirectionMarkup():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    intel_button = KeyboardButton("🎓Интелектуальная")
+    sport_button = KeyboardButton("🥋Спортивная")
+    art_button = KeyboardButton("🎨Творческая")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(intel_button, sport_button, art_button, cancel_button)
+
+    return markup
+
+# DIRECTION END
+
+# KIDS START
+
+def KidsIntel():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    
+    eng_button = KeyboardButton("🇬🇧Английский язык")
+    speech_therapist_button = KeyboardButton("🔠Логопед")
+    preparation_button = KeyboardButton("📚Подготовка к школе")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(eng_button, speech_therapist_button, preparation_button, cancel_button)
+    
+    return markup
+
+def KidsSport():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    dance_button = KeyboardButton("💃Хореография")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(dance_button, cancel_button)
+
+    return markup
+
+def KidsArt():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    development_button = KeyboardButton("🧐Развитие")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(development_button, cancel_button)
+
+    return markup
+
+# KIDS END
+
+# TEENS START
+    
+def TeensIntel():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    preparation_button = KeyboardButton("📚Подготовка к школе")
+    meth_button = KeyboardButton("🔢Ментальная алгебра")
+    exams_button = KeyboardButton("📖Подготовка к ОГЭ/ЕГЭ/ВПР")
+    eng_button = KeyboardButton("🇬🇧Английский язык")
+    arm_button = KeyboardButton("🇦🇲Армянский язык")
+    esp_button = KeyboardButton("🇪🇸Испанский язык")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(preparation_button, meth_button, exams_button, eng_button, arm_button, esp_button, cancel_button)
+
+    return markup
+
+def TeensSport():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    
+    dance_button = KeyboardButton("💃Хореография")
+    hip_hop_button = KeyboardButton("🔊Хип-Хоп")
+    karate = KeyboardButton("🥋Карате")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(dance_button, hip_hop_button, karate, cancel_button)
+
+    return markup
+
+def TeensArt():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    art_button = KeyboardButton("🎨Рисование")
+    acting_button = KeyboardButton("🎭Актерское мастерство")
+    art_education_button = KeyboardButton("🖼Исскуствоведение")
+    dance_button = KeyboardButton("💃Хореография")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(art_button, acting_button, art_education_button, dance_button, cancel_button)
+
+    return markup
+
+# TEENS END
+
+# ADULTS START
+    
+def AdultsIntel():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    eng_button = KeyboardButton("🇬🇧Английский язык")
+    arm_button = KeyboardButton("🇦🇲Армянский язык")
+    esp_button = KeyboardButton("🇪🇸Испанский язык")
+    frc_button = KeyboardButton("🇫🇷Французский язык")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(frc_button, eng_button, arm_button, esp_button, cancel_button)
+
+    return markup
+
+def AdultsArt():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    oratory_button = KeyboardButton("🎤Ораторское искуство")
+    acting_button = KeyboardButton("🎭Актерское мастерство")
+    art_education_button = KeyboardButton("🖼Рисование")
+    dance_button = KeyboardButton("💃Хореография")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(oratory_button, acting_button, art_education_button, dance_button, cancel_button)
+
+    return markup
+
+def AdultsSport():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    yoga_button = KeyboardButton("🧘Йога")
+    fitness_button = KeyboardButton("💪Фитнес")
+    cancel_button = KeyboardButton("🚫Отмена заявки🚫")
+
+    markup.add(yoga_button, fitness_button, cancel_button)
+
+    return markup
+
+# ADULTS END
+
+# REQUEST CONTACT START
+
+def RequestContact():
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+
+    request_contact_button = KeyboardButton("📱Поделиться контактом", request_contact=True)
+
+    markup.add(request_contact_button)
+
+    return markup
+
+# REQUEST CONTACT END
